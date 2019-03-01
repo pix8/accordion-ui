06560f0ea4a4003c9ce9966cf504438a5aae45b1
Jonathan Brincat <jonathan@pix8.co.uk>
2019-03-01 18:43:03 +0000

    ported to pogo.lib stack -> prep for npm release

--- example/vue/src/index.js
+++ example/vue/src/index.js
@@ -1,11 +1,9 @@
-'use strict';
-
-import Accordion from "./accordion"
+import Accordion from "@pix8/ui-accordion";
 
 
 document.documentElement.classList.remove("client__no-js");
 
-var test = new Accordion(".ui__accordion");
+new Accordion(".ui__accordion");
 //TODO: change this to work off a nominated root DOM node passed in as an argument. This abstracts the process of iteration and multiple instances and clearly separates such concerns away from the UI.
 // I think this utimately a better approach.
 //var instance1 = new Accordion(element1);
