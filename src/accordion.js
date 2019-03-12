00ecf7b2d1f1865f6507282db26b9ed9e6722923
Jonathan Brincat <jonathan@pix8.co.uk>
2019-03-12 18:38:20 +0000

    increment version number

--- src/accordion.js
+++ src/accordion.js
@@ -7,7 +7,7 @@ import './accordion.style.scss'
 //TODO: mock DOM with virtual-DOMesqe representation? use DOM node reference arrays?
 
 const 	NAME 			= "accordion",
-		VERSION			= "0.0.6";
+		VERSION			= "0.0.8";
 
 const 	className 		= {
 		ACTIVE: 		"state__active",
